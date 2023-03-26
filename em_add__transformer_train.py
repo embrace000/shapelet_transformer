1f9a72bdbaddbff9777ab4e90e5ccba4cb2b5123
embrace000 <46601124+embrace000@users.noreply.github.com>
2023-03-26 19:21:53 +0800

    Update em_add__transformer_train.py

--- em_add__transformer_train.py
+++ em_add__transformer_train.py
@@ -106,11 +106,11 @@ if __name__ == '__main__':
   data2 = np.loadtxt('data_sequence/sequence2.txt', dtype=int, delimiter=' ')
   data2 = data2.reshape(len(data2) * 20)
 
-  data3 = np.loadtxt('data_sequence/sequence1.txt', dtype=int, delimiter=' ')
+  data3 = np.loadtxt('data_sequence/sequence3.txt', dtype=int, delimiter=' ')
   data3 = data3.reshape(len(data3) * 20)
   # data3 = data3[int(len(data3) / 2):len(data3)]
 
-  data4 = np.loadtxt('data_sequence/sequence2.txt', dtype=int, delimiter=' ')
+  data4 = np.loadtxt('data_sequence/sequence4.txt', dtype=int, delimiter=' ')
   data4 = data4.reshape(len(data4) * 20)
 
 ######################################################################
@@ -184,126 +184,3 @@ history=model.fit(
 
 
 model.save('my_mode.h5')
-#
-# x=range(1,501)
-# y=history.history['val_f1_score']
-# # val_sparse_categorical_accuracy: 0.9696 - val_recall: 0.7702 - val_precision
-# # 绘图
-# plt.figure()
-# lw = 2
-# plt.plot(x, y, color='darkorange',
-#          lw=lw, label='Without behavior unit feathers (area = %0.2f)')
-#
-# # plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--') ':'
-#
-#
-# x_major_locator=MultipleLocator(1)
-# #把x轴的刻度间隔设置为1，并存在变量里
-# y_major_locator=MultipleLocator(10)
-# #把y轴的刻度间隔设置为10，并存在变量里
-# ax=plt.gca()
-# #ax为两条坐标轴的实例
-# ax.xaxis.set_major_locator(x_major_locator)
-# #把x轴的主刻度设置为1的倍数
-# ax.yaxis.set_major_locator(y_major_locator)
-# #把y轴的主刻度设置为10的倍数
-#
-#
-#
-# plt.xlim([0, 501])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('epoch')
-# plt.ylabel('F1-score')
-# plt.title('')
-# plt.legend(loc="lower right")
-# # plt.savefig('roc.png',)
-# plt.show()
-#
-
-
-
-
-
-
-
-
-
-
-'''
-  temp1=np.array([])
-  temp2 = np.array([])
-  temp3 = np.array([])
-  temp4= np.array([])
-  temp5 = np.array([])
-
-  for i in tqdm (range(int(0.2*int(len(data1)/400)))) :
-      l1 =data1 [i*400:(i+1)*400]
-
-      for j in range(len(shapelets)):
-         l2  =   shapelets[j]
-         length_,lists = lcs.calculate_LCS(l1, l2)
-         if len(lists)==5 :
-            temp1 = np.append( temp1,j).astype(int)
-            temp2 = np.append(temp2 ,np.where(l1 == shapelets[j][0])[0][0]).astype(int)
-            temp_test = np.where(l1 == shapelets[j][0])[0]
-            print(np.where(l1 == shapelets[j][0])[0])
-
-
-      if len(temp1>0):
-          index=np.argsort(temp2)
-          posi_temp=  np.sort(temp2)
-          for k in range(len(index)) :
-              temp3=np.append(temp3, temp1[index[k]]).astype(int)
-              temp4 = np.append(temp4,  posi_temp[k]).astype(int)
-              # print(temp3)
-      temp1 = np.array([])
-      temp2 = np.array([])
-
-  temp5 = numpy.diff(temp4)
-  temp5 =  np.append( [0],temp5).astype(int)
-
-  # 对temp3 序列 和 temp5序列差值进行操作 差值小于20认为重合 大于20认为不重合
-  seqlen = 20 #input_size
-  temp6= np.array([])
-  temp7 = np.ones(seqlen)
-
-  def bound_diff(index):
-      if index > 20:
-          return 0
-      else:
-          return 1
-
-  for i in range(len(temp3)):
-      if i == 0:
-          temp6 = np.append(temp6, temp3[i])
-      else:
-          if bound_diff(temp5[i]) == bound_diff(temp5[i - 1]):
-              temp6 = np.append(temp6, temp3[i])
-          else:
-              if len(temp6) < seqlen:    #补齐
-                  temp6 = np.pad(temp6, (0, seqlen - len(temp6)), 'constant', constant_values=(0, -1))
-              else:
-                  temp6 = temp6[0:seqlen]   #截断
-              #print(temp6)
-              temp7 = np.vstack((temp7, temp6))
-              temp6 = np.append([], temp3[i])
-
-  temp7 = np.delete(temp7, 0, axis=0)
-  data = pd.DataFrame(temp7)
-  data.to_csv('transform3-0.4.txt', sep=' ', index=False, header=False)
-'''
-
-
-  # print(temp)
-  # # get two lists
-  # l1 = [5, 3, 4, 212, 7, 6, 1, 7, 256, 189, 7, 167, 5]
-  # l2 = [212, 7, 256, 5]
-  # lists = lcs.lcs(l1, l2)
-  # for l in lists:
-  #     print(l)
-
-##########
-  # l1 =np.array( [5, 3, 4, 212, 7, 6, 1, 7, 256, 189, 7, 167, 5])
-  # m=np.where(l1==212)
-  # m=m[0][0]
-  # print(m)
